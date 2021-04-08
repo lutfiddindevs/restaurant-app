439f5c54bbc288664d2698f26015b6125f7c251f
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-08 23:41:58 +0500

    create category page

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form action="{{ route('category.store') }}" method="post">
+                @csrf
+                <div class="card">
+                    <div class="card-header">Manage Food Category</div>
+
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                           <button class="btn btn-outline-primary">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
