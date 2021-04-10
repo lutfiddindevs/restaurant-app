5e2ba60df5eba93689077b1625f2a0c26e921fe4
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-04-10 11:41:46 +0500

    validation

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if(Session::has('message'))
+                <div class="alert alert-success">
+                    {{ Session::get('message') }}
+                </div>
+            @endif
             <form action="{{ route('category.store') }}" method="post">
                 @csrf
                 <div class="card">
@@ -12,7 +17,12 @@
                     <div class="card-body">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" name="name" class="form-control">
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                         <div class="form-group">
                            <button class="btn btn-outline-primary">Submit</button>
